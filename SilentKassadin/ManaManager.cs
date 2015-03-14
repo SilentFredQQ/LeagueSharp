7b0532525c5a26b1ba24926fc3b4cb7034bbae6b
Simone QQ <simo.wasabi.fiore@gmail.com>
2015-03-14 13:45:26 +0100

    Added SilentKassadin
    5.5 work

---
+++ SilentKassadin/ManaManager.cs
@@ -0,0 +1,64 @@
+﻿using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace SilentKassadin
+{
+    internal class ManaManager
+    {
+        private readonly Obj_AI_Hero _player;
+        private Menu _menu;
+
+        public ManaManager()
+        {
+            _player = ObjectManager.Player;
+        }
+
+        /// <summary>
+        ///     Adds the options to the main menu
+        /// </summary>
+        /// <param name="attachMenu"></param>
+        public void AddToMenu(ref Menu attachMenu)
+        {
+            _menu = attachMenu;
+            CreateMenu();
+
+            //Game.PrintChat(string.Format("{0} loaded by {1}", "Mana Manager", "iJabba"));
+        }
+
+        /// <summary>
+        ///     Actually creates the menu
+        /// </summary>
+        public void CreateMenu()
+        {
+            var manaMenu = new Menu("Mana Manager", "manaManager");
+            {
+                manaMenu.AddItem(new MenuItem("minHarassMana", "Min Mana for harass").SetValue(new Slider(40)));
+                manaMenu.AddItem(new MenuItem("minLaneclearMana", "Min Mana for wave & jungleclear").SetValue(new Slider(40)));
+            }
+
+            _menu.AddSubMenu(manaMenu);
+        }
+
+        public bool CanUseSpell(Spell spell)
+        {
+            return _player.Mana >= _player.Spellbook.GetSpell(spell.Slot).ManaCost;
+        }
+
+        public bool CanDoCombo()
+        {
+            return _player.Mana >=
+                   _player.Spellbook.GetSpell(SpellSlot.Q).ManaCost + _player.Spellbook.GetSpell(SpellSlot.W).ManaCost +
+                   _player.Spellbook.GetSpell(SpellSlot.E).ManaCost + _player.Spellbook.GetSpell(SpellSlot.R).ManaCost;
+        }
+
+        public bool CanHarass()
+        {
+            return !(_player.ManaPercentage() <= _menu.Item("minHarassMana").GetValue<Slider>().Value);
+        }
+
+        public bool CanLaneclear()
+        {
+            return !(_player.ManaPercentage() <= _menu.Item("minLaneclearMana").GetValue<Slider>().Value);
+        }
+    }
+}
---
+++ SilentKassadin/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SilentKassadin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SilentKassadin")]
+[assembly: AssemblyCopyright("Copyright © SilentFredQQ 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7430f8de-a07d-473d-b374-9ef5a6f398f2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
